005d82173549cf724da16cea9424f23aac7b6148
Saurabh Palatkar <spalatkar@gmail.com>
2021-03-02 20:08:18 +0530

    fix: styling corrected

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.css'],
+  styleUrls: ['./pages.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PagesComponent implements OnInit {
