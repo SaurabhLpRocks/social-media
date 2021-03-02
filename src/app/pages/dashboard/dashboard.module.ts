dbf4416e10095c575acd0829ef650103a1cdfeab
Saurabh Palatkar <spalatkar@gmail.com>
2021-03-02 20:08:36 +0530

    feat: comp to show users list

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,10 +3,11 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, DashboardRoutingModule],
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, UsersListComponent],
   providers: [],
 })
 export class DashboardModule {}
